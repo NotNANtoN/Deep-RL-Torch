Fixed central optimization. Added mixed precision training (slows down training by a factor of 10 atm)

networks.py
@@ -553,7 +553,7 @@ class TempDiffNet(OptimizableNet):
         return apply_layers(x, self.layers_TD, self.act_functs_TD)
 
     def calculate_next_state_values(self, non_final_next_state_features, non_final_mask, actor=None, use_target_net=True):
-        next_state_values = torch.zeros(len(non_final_mask), 1, device=self.device)
+        next_state_values = torch.zeros(len(non_final_mask), 1, device=self.device, dtype=non_final_next_state_features.dtype)
         if non_final_next_state_features is None:
             return next_state_values
         with torch.no_grad():

policies.py
@@ -10,6 +10,10 @@ from exp_rep import ReplayBuffer, PrioritizedReplayBuffer
 from networks import Q, V, Actor, ProcessState, ProcessStateAction
 from util import *
 import copy
+try:
+    from apex import amp
+except:
+    print("WARNING: apex could not be imported.")
 
 
 # This is the interface for the agent being trained by a Trainer instance
@@ -53,6 +57,7 @@ class Agent(AgentInterface):
         self.hyperparameters = hyperparameters
 
         self.optimize_centrally = hyperparameters["optimize_centrally"]
+        self.mixed_precision_train = hyperparameters["mixed_precision_train"] and torch.cuda.is_available()
         self.use_actor_critic = hyperparameters["use_actor_critic"]
         self.save_path = hyperparameters["save_path"]
         self.save_threshold = hyperparameters["save_percentage"]
@@ -70,13 +75,19 @@ class Agent(AgentInterface):
                 params += self.F_sa.get_updateable_params()
             general_lr = hyperparameters["general_lr"]
             self.optimizer = hyperparameters["optimizer"](params, lr=general_lr)
+            if self.mixed_precision_train:
+                _, self.optimizer = amp.initialize([], self.optimizer)
 
     def init_feature_extractors(self):
         F_s = ProcessState(self.env, self.log, self.device, self.hyperparameters)
+        if self.mixed_precision_train:
+            F_s = amp.initialize(F_s, verbosity=0)
         F_sa = None
         if self.use_actor_critic:
             state_feature_len = F_s.layers_merge[-1].out_features
             F_sa = ProcessStateAction(state_feature_len, self.env, self.log, self.device, self.hyperparameters)
+            if self.mixed_precision_train:
+                F_sa = amp.initialize(F_sa, verbosity=0)
         return F_s, F_sa
 
     def create_policy(self):
@@ -118,7 +129,11 @@ class Agent(AgentInterface):
 
         if self.optimize_centrally:
             self.optimizer.zero_grad()
-            loss.backward()
+            if self.mixed_precision_train:
+                with amp.scale_loss(loss, self.optimizer) as loss_scaled:
+                    loss_scaled.backward()
+            else:
+                loss.backward()
             self.F_s.scale_gradient()
             if self.F_sa is not None:
                 self.F_sa.scale_gradient()
@@ -279,6 +294,7 @@ class BasePolicy:
             self.state_action_feature_len = F_sa.layers_merge[-1].out_features
 
         # Set up Networks:
+        self.mixed_precision_train = hyperparameters["mixed_precision_train"] and torch.cuda.is_available()
         self.nets = []
         self.actor, self.Q, self.V = self.init_actor_critic(self.F_s, self.F_sa)
 
@@ -289,8 +305,7 @@ class BasePolicy:
 
     def boltzmann_exploration(self, action):
         pass
-
-    # TODO: implement boltzman exploration
+        # TODO: implement boltzman exploration
 
     def explore_discrete_actions(self, action):
         if self.boltzmann_exploration_temp > 0:
@@ -373,12 +388,16 @@ class BasePolicy:
         Q_net = None
         if not (self.use_CACLA_V and not self.use_QVMAX):
             Q_net = Q(input_size, self.env, F_s, F_sa, self.device, self.log, self.hyperparameters)
+            if self.mixed_precision_train:
+                Q_net = amp.initialize(Q_net, verbosity=0)
             self.nets.append(Q_net)
 
         V_net = None
         if self.use_QV or self.use_QVMAX or (self.use_actor_critic and self.use_CACLA_V):
             # Init Networks:
             V_net = V(self.state_feature_len, self.env, F_s, None, self.device, self.log, self.hyperparameters)
+            if self.mixed_precision_train:
+                V_net = amp.initialize(V_net, verbosity=0)
             self.nets.append(V_net)
         return Q_net, V_net
 
@@ -648,6 +667,8 @@ class ActorCritic(BasePolicy):
 
     def init_actor(self, Q, V, F_s):
         actor = Actor(F_s, self.env, self.log, self.device, self.hyperparameters)
+        if self.mixed_precision_train:
+            actor = amp.initialize(actor, verbosity=0)
         actor.Q = Q
         actor.V = V
         self.nets.append(actor)
@@ -721,13 +742,10 @@ class Q_Policy(BasePolicy):
             self.V.save(path + "V/")
 
     def get_updateable_params(self):
-        params = None
+        params = []
         for net in self.nets:
             new_params = list(net.get_updateable_params())
-            if params is None:
-                params = new_params
-            else:
-                params += new_params
+            params += new_params
         return params
 
 
@@ -1087,13 +1105,6 @@ class MineRLMovePolicy(MineRLPolicy):
         return action_q_vals.unsqueeze(0)
 
 
-
-
-    def test_some_stuff(self):
-        output = [1,2,3]
-        return output
-
-
     def get_action_idxs_for_policy(self, policy, action_dicts):
         action_idxs = torch.zeros(len(action_dicts), device=self.device).long()
         for dict_idx, action_dict in enumerate(action_dicts):
@@ -1123,6 +1134,7 @@ class MineRLMovePolicy(MineRLPolicy):
 
     def optimize_networks(self, transitions):
         error = 0
+        loss = 0
         # Save actions:
         original_actions = transitions["action_argmax"].clone()
         # Transform actions in dicts:
@@ -1130,9 +1142,11 @@ class MineRLMovePolicy(MineRLPolicy):
         for policy in self.policies:
             action_idxs = self.get_action_idxs_for_policy(policy, action_dicts)
             transitions["action_argmax"] = action_idxs
-            error += policy.optimize_networks(transitions)
+            error_it, loss_it = policy.optimize_networks(transitions)
+            error += error_it
+            loss += loss_it
         transitions["action_argmax"] = original_actions
-        return error
+        return error, loss
 
     def update_targets(self, n_steps, train_fraction=None):
         for policy in self.policies:
@@ -1156,7 +1170,10 @@ class MineRLMovePolicy(MineRLPolicy):
             policy.save(path)
 
     def get_updateable_params(self):
-        return [policy.get_updateable_params() for policy in self.policies]
+        params = []
+        for policy in self.policies:
+            params += list(policy.get_updateable_params())
+        return params
 
 
 
@@ -1409,8 +1426,10 @@ class MineRLHierarchicalPolicy(MineRLPolicy):
 
     def get_updateable_params(self):
         if self.decider is not None:
-            params = [self.decider.get_updateable_params()]
+            params = list(self.decider.get_updateable_params())
         else:
             params = []
-        params.extend([policy.get_updateable_params() for policy in self.lower_level_policies])
+        for policy in self.lower_level_policies:
+            params += list(policy.get_updateable_params())
+        #params.extend([policy.get_updateable_params() for policy in self.lower_level_policies])
         return params

trainer.py
@@ -1,3 +1,5 @@
+import collections
+
 import minerl
 import gym
 
@@ -52,7 +54,7 @@ class Trainer:
             exclude_keys = hyperparameters["exclude_keys"]
             action_wrapper = hyperparameters["action_wrapper"]
 
-            self.env = action_wrapper(self.env, always_keys=always_keys, exclude_keys=exclude_keys)
+            self.env = action_wrapper(self.env, always_keys=always_keys, exclude_keys=exclude_keys, env_name=env_name)
             print("Num actions after wrapping: ", self.env.action_space.n)
 
         if hyperparameters["max_episode_steps"] > 0:
@@ -69,7 +71,12 @@ class Trainer:
             self.max_steps_per_episode = 0
         self.reward_std = hyperparameters["reward_std"]
         self.use_exp_rep = hyperparameters["use_exp_rep"]
+
+        # Exploration
+        self.use_elig_traces = hyperparameters["use_efficient_traces"]
         self.n_initial_random_actions = hyperparameters["n_initial_random_actions"]
+        self.explore_until_reward = hyperparameters["explore_until_reward"]
+
         self.updates_per_step = hyperparameters["network_updates_per_step"]
 
         # copied from Old class:
@@ -79,6 +86,7 @@ class Trainer:
         #    self.normalizer = Normalizer(self.state_len)
         # else:
         #    self.normalizer = None
+        self.log_freq = hyperparameters["log_freq"]
 
         # Show proper tqdm progress if possible:
         self.disable_tqdm = hyperparameters["tqdm"] == 0
@@ -204,8 +212,6 @@ class Trainer:
             sample = (state, action, reward, next_state, done)
             data.append(sample)
 
-            #if len(data) > 10000:
-            #    break
         return data
 
         # TODO: apply frameskip here! (if used)
@@ -279,26 +285,55 @@ class Trainer:
             state = apply_rec_to_dict(self.prep_for_GPU, state)
         #TODO: move these preparations to one place, add general env wrapper for easy envs
 
-        # Fill exp replay buffer so that we can start training immediately:
-        for _ in tqdm(range(n_steps), disable=self.disable_tqdm):
+        rewards = collections.defaultdict(int)
 
+        # Fill exp replay buffer so that we can start training immediately:
+        pbar = tqdm(disable=self.disable_tqdm)
+        i = 0
+        done = True
+        done_count = 0
+        do_break = False
+        while True:
+            pbar.update(1)
             # To initialize the normalizer:
             if self.normalize_observations:
-                #pass
-                self.policy.F_s.observe(state)
+                self.agent.F_s.observe(state)
                 # TODO: normalize (observe) actions too
             action, next_state, reward, done = self._act(self.env, state, store_in_exp_rep=True, render=False,
                                                          explore=True, fully_random=True)
 
             state = next_state
             if done:
+                done_count += 1
                 state = self.env.reset()
                 
-            if not isinstance(state, dict):
-                state = torch.tensor([state], dtype=torch.float)
-                state = self.prep_for_GPU(state)
+                if not isinstance(state, dict):
+                    state = torch.tensor([state], dtype=torch.float)
+                    state = self.prep_for_GPU(state)
+                else:
+                    state = apply_rec_to_dict(self.prep_for_GPU, state)
+
+
+            if self.explore_until_reward and not do_break:
+                if isinstance(reward, torch.FloatTensor) or isinstance(reward, torch.LongTensor):
+                    reward = reward.item()
+                rewards[reward] += 1
+                if len(rewards) > 1:
+                    print("Encountered a new reward value. Rewards; ", rewards)
+                    do_break = True
             else:
-                state = apply_rec_to_dict(self.prep_for_GPU, state)
+                i += 1
+                if  i >= n_steps:
+                    do_break = True
+
+            # For eligibility traces we need to complete at least one episode to properly start training
+            if do_break:
+                if self.use_elig_traces:
+                    if done_count >= 1:
+                        break
+                else:
+                    break
+
 
         print("Done with filling replay buffer.")
         print()
@@ -316,7 +351,7 @@ class Trainer:
         else:
             action, raw_action = self.agent.exploit(state)
 
-        self.log.add("ActionIdx", action, make_distribution=True, skip_steps=10000)
+        self.log.add("ActionIdx", action, make_distribution=True, skip_steps=self.log_freq)
 
         # Apply the action:
         next_state, reward, done, _ = env.step(action)
@@ -417,7 +452,7 @@ class Trainer:
         # Do the actual training:
         print("Start training in the env:")
         time_after_optimize = None
-        pbar = tqdm(total=n_steps, desc="Total Training", disable=self.disable_tqdm)
+        #pbar = tqdm(total=n_steps, desc="Total Training", disable=self.disable_tqdm)
         train_fraction = calc_train_fraction(n_steps, steps_done, n_episodes, i_episode, n_hours, start_time)
 
         while train_fraction < 1:
@@ -455,7 +490,7 @@ class Trainer:
                 non_optimize_time = 0
                 if time_after_optimize is not None:
                     non_optimize_time = time_before_optimize - time_after_optimize
-                self.log.add("Non-Optimize_Time", non_optimize_time, skip_steps=10000, store_episodic=True)
+                self.log.add("Non-Optimize_Time", non_optimize_time, skip_steps=self.log_freq, store_episodic=True)
 
                 num_updates = int(self.updates_per_step) if self.updates_per_step >= 1\
                                                     else n_steps % int(1 / self.updates_per_step) == 0
@@ -468,9 +503,10 @@ class Trainer:
                 time_after_optimize = time.time()
 
                 # Log reward and time:
-                self.log.add("Train_Reward", reward.item(), skip_steps=10000, store_episodic=True)
-                self.log.add("Train_Sum_Episode_Reward", np.sum(self.log.get_episodic("Train_Reward")), skip_steps=10000)
-                self.log.add("Optimize_Time", time_after_optimize - time_before_optimize, skip_steps=10000,
+                self.log.add("Train_Reward", reward.item(), skip_steps=self.log_freq, store_episodic=True)
+                self.log.add("Train_Sum_Episode_Reward", np.sum(self.log.get_episodic("Train_Reward")),
+                             skip_steps=self.log_freq)
+                self.log.add("Optimize_Time", time_after_optimize - time_before_optimize, skip_steps=self.log_freq,
                              store_episodic=True)
 
                 if render:
@@ -479,7 +515,7 @@ class Trainer:
                 self.log.step()
                 if done or (n_steps and steps_done >= n_steps) or (n_episodes and i_episode >= n_episodes) or\
                         (n_hours and (time.time() - start_time) / 360 >= n_hours):
-                    pbar.update(t)
+                    #pbar.update(t)
 
                     self.log.add("Return", np.sum(self.log.get_episodic("Train_Reward")), steps=i_episode, store_episodic=True)
                     if verbose:
@@ -492,6 +528,6 @@ class Trainer:
         self.agent.update_targets(steps_done, train_fraction=1.0)
         print('Done.')
         self.env.close()
-        pbar.close()
+        #pbar.close()
         return i_episode
 