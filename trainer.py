22dee44b4771172477ae7978a307274bd57df26c
Anton <antonwiehe@gmail.com>
2019-12-16 17:29:13 +0100

    m

--- trainer.py
+++ trainer.py
@@ -125,7 +125,7 @@ class Trainer:
             exclude_keys = hyperparameters["exclude_keys"]
             action_wrapper = hyperparameters["action_wrapper"]
 
-            env = action_wrapper(env, always_keys=always_keys, exclude_keys=exclude_keys, env_name=env_name)
+            env = action_wrapper(env, always_keys=always_keys, exclude_keys=exclude_keys, env_name=self.env_name)
         return env
 
     def reset(self):
@@ -398,7 +398,7 @@ class Trainer:
                 reward_sum += reward
                 if done:
                     break
-        reward_sum /= 4
+        reward_sum /= self.eval_rounds
         return reward_sum
 
 
@@ -436,6 +436,10 @@ class Trainer:
             #plot_rewards(self.log.storage["Test_Env Reward"], "Test_Env Reward")
             #plot_rewards(self.log.storage["Return"], "Return", xlabel="Episodes")
         print()
+
+    def close(self):
+        self.env.close()
+        #self.log.close()
 
     def run(self, n_hours=0.0, n_episodes=0, n_steps=0, verbose=False, render=False, on_server=True):
         assert (bool(n_steps) ^ bool(n_episodes) ^ bool(n_hours))
@@ -485,7 +489,7 @@ class Trainer:
             for t in tqdm(count(), desc="Episode Progress", total=self.tqdm_episode_len, disable=self.disable_tqdm):
                 steps_done += 1
 
-                # Act in exploratory env:
+                # Act in train env:
                 action, next_state, reward, done = self._act(self.env, state, render=render, store_in_exp_rep=True,
                                                              explore=True)
 
